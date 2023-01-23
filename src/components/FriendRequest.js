f8ac05057912df911acaf3dcff81a6d2b3435f1a
A B M Shawon Islam <55059592+ABMshawonislam-dev@users.noreply.github.com>
2023-01-23 23:49:11 +0600

    friend request done without accept and reject

--- src/components/FriendRequest.js
+++ src/components/FriendRequest.js
@@ -1,155 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useSelector } from "react-redux";
+import Alert from "@mui/material/Alert";
 
 const FriendRequest = () => {
+  const db = getDatabase();
+
+  let [freq, setFreq] = useState([]);
+
+  let data = useSelector((state) => state);
+
+  console.log(data.userdata.userInfo.uid);
+
+  useEffect(() => {
+    const starCountRef = ref(db, "frienrequest");
+    onValue(starCountRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (item.val().receiverid == data.userdata.userInfo.uid) {
+          arr.push(item.val());
+        }
+      });
+      setFreq(arr);
+    });
+  }, []);
+
   return (
     <div className="groupholder">
       <div className="titleholder">
         <h3>Friend Request</h3>
       </div>
       <div className="boxholder">
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Accept</button>
-            <button className="boxbtn">Reject</button>
-          </div>
-        </div>
+        {freq.length > 0 ? (
+          freq.map((item) => (
+            <div className="box">
+              <div className="boximgholder">
+                <img src="assets/profile.png" />
+              </div>
+              <div className="title">
+                <h3>{item.sendername}</h3>
+                <p>Hi Guys, Wassup!</p>
+              </div>
+              <div>
+                <button className="boxbtn">Accept</button>
+                <button className="boxbtn">Reject</button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <Alert style={{ marginTop: "20px" }} variant="filled" severity="info">
+            No Friend Request
+          </Alert>
+        )}
       </div>
     </div>
   );
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,29 +1,48 @@
 import React, { useEffect, useState } from "react";
-import { getDatabase, ref, onValue} from "firebase/database";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
 import { useSelector } from "react-redux";
 
 const UserList = () => {
-  let db = getDatabase()
-  let [userlist,setUserlist] = useState([])
+  let db = getDatabase();
+  let [userlist, setUserlist] = useState([]);
+  let [freq, setFreq] = useState([]);
 
-  let data = useSelector(state=>state)
+  let data = useSelector((state) => state);
 
-  console.log(data.userdata.userInfo.uid)
+  console.log(data.userdata.userInfo.uid);
 
-
-  useEffect(()=>{
-    const usersRef = ref(db, 'users');
+  useEffect(() => {
+    const usersRef = ref(db, "users");
     onValue(usersRef, (snapshot) => {
-      let arr = []
-      snapshot.forEach(item=>{
-        if(data.userdata.userInfo.uid != item.key){
-          arr.push(item.val())
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (data.userdata.userInfo.uid != item.key) {
+          arr.push({ ...item.val(), id: item.key });
         }
-      })
-      setUserlist(arr)
+      });
+      setUserlist(arr);
     });
-  },[])
+  }, []);
 
+  useEffect(() => {
+    const usersRef = ref(db, "frienrequest");
+    onValue(usersRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        arr.push(item.val().receiverid + item.val().senderid);
+      });
+      setFreq(arr);
+    });
+  }, []);
+
+  let handleFriendRequest = (info) => {
+    set(push(ref(db, "frienrequest")), {
+      sendername: data.userdata.userInfo.displayName,
+      senderid: data.userdata.userInfo.uid,
+      receivername: info.displayName,
+      receiverid: info.id,
+    });
+  };
 
   return (
     <div className="groupholder">
@@ -31,22 +50,30 @@ const UserList = () => {
         <h3>Users List</h3>
       </div>
       <div className="boxholder">
-        {userlist.map(item=>(
+        {userlist.map((item) => (
           <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
-          </div>
-          <div className="title">
-            <h3>{item.displayName}</h3>
-            <p>{item.email}</p>
-          </div>
-          <div>
-            <button className="boxbtn">Send Request</button>
-          </div>
+            <div className="boximgholder">
+              <img src="assets/profile.png" />
+            </div>
+            <div className="title">
+              <h3>{item.displayName}</h3>
+              <p>{item.email}</p>
+            </div>
+            <div>
+              {freq.includes(item.id + data.userdata.userInfo.uid) ||
+              freq.includes(data.userdata.userInfo.uid + item.id) ? (
+                <button className="boxbtn">Pending</button>
+              ) : (
+                <button
+                  onClick={() => handleFriendRequest(item)}
+                  className="boxbtn"
+                >
+                  Send Request
+                </button>
+              )}
+            </div>
           </div>
         ))}
-       
-       
       </div>
     </div>
   );
